6d20de8616fe0bf09e38a371461c5ea7a4bcecf1
syuheifujita <syuheifujita0117@gmail.com>
2020-08-12 17:10:00 +0900

    #1 不要なmodelのファイルを削除

--- app/src/main/java/com/syuheifujita/android_intern_challenge_wantedly/model/ItemResponse.kt
+++ app/src/main/java/com/syuheifujita/android_intern_challenge_wantedly/model/ItemResponse.kt
@@ -5,3 +5,24 @@ import java.io.Serializable
 data class ItemResponse (
     val data: List<Data>
 )
+
+data class Data (
+    val title: String,
+    val looking_for: String,
+    val image: Image,
+    val company: Company
+): Serializable
+
+data class Image (
+    val original: String
+): Serializable
+
+data class Company (
+    val name: String,
+    val url: String,
+    val avatar: Avatar
+): Serializable
+
+data class Avatar (
+    val original: String
+): Serializable
--- app/src/main/java/com/syuheifujita/android_intern_challenge_wantedly/model/Tags.kt
+++
@@ -1,7 +0,0 @@
-package com.syuheifujita.android_intern_challenge_wantedly.model
-
-import java.io.Serializable
-
-data class Tags (
-    val image: String? = null
-): Serializable
